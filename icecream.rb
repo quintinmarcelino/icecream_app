8f72d761bfa32188f9674b9da268f264d1d1e14e
Quintin <quintinmarcelino@gmail.com>
2014-09-24 19:53:14 -0400

    Homework Ice Cream File

---
+++ icecream.rb
@@ -0,0 +1,5 @@
+#Oh man, Ice Cream! My favorite flavor is easily
+#Coffee Hydrox, especially from Tuscanini's. 
+#Coffee was always my favorite flavor growing up and
+#adding Oreo chunks only makes it WAY BETTER.
+  
